7657b0428d68f98765d6c16e0237d14c8100ec98
richardo2016 <richardo2016@gmail.com>
2018-01-29 13:27:00 +0800

    add lib content, test cases and README.

---
+++ test/index.spec.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env fibjs
+
+var test = require('test');
+test.setup();
+
+const fs = require('fs')
+
+const pug = require('../')
+
+describe('测试 pug.compile', () => {
+  it('测试一段 raw text', () => {
+    const pugRaw = 'div'
+    const html = pug.compile(pugRaw)()
+    assert.equal('<div></div>', html);
+  })
+
+  it('测试一段带变量的 raw text', () => {
+    const pugRaw = 'div\n\t!= testVar'
+    const locals = {testVar: '测试变量'}
+    const html = pug.compile(pugRaw)(locals)
+    assert.equal('<div>测试变量</div>', html);
+  })
+
+  it('测试另一段从文件里读取的 raw text', () => {
+    const pugRaw = fs.readTextFile('./fib-pug.1.pug')
+    const html = pug.compile(pugRaw)()
+    assert.equal('<div>我是一个 pug 文件, 在 fibjs 的驱动下跑 pug 包.</div>', html);
+  })
+})
+
+describe('测试 pug.render', () => {
+  it('测试一段 raw text', () => {
+    const pugRaw = 'div'
+    const html = pug.render(pugRaw)
+    assert.equal('<div></div>', html);
+  })
+
+  it('测试另一段从文件里读取的 raw text', () => {
+    const pugRaw = fs.readTextFile('./fib-pug.1.pug')
+    const html = pug.render(pugRaw)
+    assert.equal('<div>我是一个 pug 文件, 在 fibjs 的驱动下跑 pug 包.</div>', html);
+  })
+})
+
+describe('测试 pug.renderFile', () => {
+  it('basic', () => {
+    const html = pug.renderFile('./fib-pug.1.pug')
+    assert.equal('<div>我是一个 pug 文件, 在 fibjs 的驱动下跑 pug 包.</div>', html);
+  })
+})
+
+test.run()
